53e6b47db49ce861a9290bacbfd8b6d787a0f084
Arun <arun11.kb@gmail.com>
2025-03-25 17:04:33 +0530

    k8s url added in axios config

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5001'
+const BASE_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:5001'
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL
